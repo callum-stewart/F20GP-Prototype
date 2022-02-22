9992582389f6c68f3d1f0cabef261e0aeacf5f9e
kd41 <kyled073@gmail.com>
2022-02-22 20:02:12 +0000

    Light Circuits done for section A

--- Ratcatcher/Assets/LightSwitch.cs
+++ Ratcatcher/Assets/LightSwitch.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class LightSwitch : Interactable
 {
+    public Light[] circuit;
+    bool powered;
+    bool isOn;
+
+    private void Awake()
+    {
+        isOn = true;
+        changeLights();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (interactive && Input.GetButtonDown("Interact"))
         {
             FindObjectOfType<AudioManager>().Play("Flashlight");
+            changeLights();
         }
+    }
+
+    // switch on or off lights in circuit
+    void changeLights()
+    {
+        foreach(Light l in circuit)
+        {
+            if (isOn)
+                l.intensity = 0;
+            else
+                l.intensity = 1;
+
+        }
+
+        isOn = !isOn;
     }
 }
