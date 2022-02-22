3c06fc29daa60ed35050bd0398521c3914a7fb89
kd41 <kyled073@gmail.com>
2022-02-22 18:10:17 +0000

    Interaction Lever works with sound

--- Ratcatcher/Assets/Scripts/Interactables/Interactable.cs
+++ Ratcatcher/Assets/Scripts/Interactables/Interactable.cs
@@ -6,11 +6,20 @@ public class Interactable : MonoBehaviour
 {
     public string displayText;
 
+    // display the control text
     private void OnTriggerEnter(Collider other)
     {
         FindObjectOfType<UserInterface>().setInfo(displayText, 10f);
     }
 
+    // interaction
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Interact"))
+            FindObjectOfType<AudioManager>().Play("Voice1");
+    }
+
+    // close text
     private void OnTriggerExit(Collider other)
     {
         FindObjectOfType<UserInterface>().setInfo("");
