a77a0146b64488a38b364dac245d912f70ce4276
kd41 <kyled073@gmail.com>
2022-02-24 03:09:15 +0000

    Created new spawn points

--- Ratcatcher/Assets/GameManager.cs
+++ Ratcatcher/Assets/GameManager.cs
@@ -4,24 +4,29 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject ratNestPrefab;
-    private Vector3[] spawnPoints = {
+    private Vector3[] points = {
         new Vector3(5.5f, .15f, 0f),    // Reception
         new Vector3(-11f, .15f, 2f),    // Breakroom
-        new Vector3(9.5f, .15f, 18.5f), // Offices
-        new Vector3(9.5f, .15f, 31.5f), // Offices 2nd
-        new Vector3(18f, .15f, 37.5f),  // Security
-        new Vector3(5.5f, .15f, 38.5f), // Holding
-        new Vector3(-8f, .15f, 46f),    // Delivery
-        new Vector3(-14.5f, .15f, 53f)    // Head Office
+        new Vector3(2f, .15f, 12f),     // GN Toilet
+        new Vector3(9f, .15f, 16f),     // Janitorial Cupboard
+        new Vector3(-4f, .15f, 25f),    // Offices 
+        new Vector3(-3f, .15f, 36f),    // lab
+        new Vector3(17f, .15f, 56f),    // holding
+        new Vector3(16f, .15f, 55f),     // head office
+        new Vector3(-7f, .15f, 44f),    // Delivery ROAM ONLY
+        new Vector3(7f, .15f, 58f),     // Hallway C ROAM ONLY
+        new Vector3(5f, .15f, 14f),     // Prep Room ROAM ONLY
+        new Vector3(17f, .15f, 38f)     // security ROAM ONLY
     };
 
     public void Start()
     {
-        for(int i = 0; i < spawnPoints.Length; i++)
+        for(int i = 0; i < points.Length-4; i++)
         {
             // create the nest object
-            GameObject newRatRef = Instantiate(ratNestPrefab, spawnPoints[i], ratNestPrefab.transform.rotation);
+            GameObject newRatRef = Instantiate(ratNestPrefab, points[i], ratNestPrefab.transform.rotation);
             RatNest nest = newRatRef.GetComponent<RatNest>();
+            nest.points = points;
         }
         
     }
--- Ratcatcher/Assets/Scripts/RatNest.cs
+++ Ratcatcher/Assets/Scripts/RatNest.cs
@@ -7,29 +7,10 @@ public class RatNest : MonoBehaviour
     public RatCatcher RatCatcher;
     [SerializeField]
     List<Rat> rats = new List<Rat>();
-    //Rat leader;
-
     int maxRat = 36;
-    private Vector3[] spawnPoints = {
-        new Vector3(5.5f, .15f, 0f),    // Reception
-        new Vector3(9.5f, .15f, 18.5f), // Offices
-        new Vector3(9.5f, .15f, 31.5f), // Offices 2nd
-        new Vector3(18f, .15f, 37.5f),  // Security
-        new Vector3(5.5f, .15f, 38.5f), // Holding
-        new Vector3(-8f, .15f, 46f),    // Delivery
-        new Vector3(-14.5f, .15f, 53f)    // Head Office
-    };
-    public Vector3[] roamingPoints = {
-        new Vector3(5.5f, .15f, 0f),    // Reception
-        new Vector3(9.5f, .15f, 18.5f), // Offices
-        new Vector3(9.5f, .15f, 31.5f), // Offices 2nd
-        new Vector3(18f, .15f, 37.5f),  // Security
-        new Vector3(5.5f, .15f, 38.5f), // Holding
-        new Vector3(-8f, .15f, 46f),    // Delivery
-        new Vector3(-14.5f, .15f, 53f)    // Head Office
-    };
+    public Vector3[] points;
 
-    private void Awake()
+    private void Start()
     {
         RatCatcher = FindObjectOfType<RatCatcher>();
         for (int i = 0; i < maxRat; i++)
@@ -46,7 +27,7 @@ public class RatNest : MonoBehaviour
     void createRat()
     {
         // create the rat object
-        GameObject newRatRef = Instantiate(ratPrefab, generateRandomPoint(spawnPoints), ratPrefab.transform.rotation);
+        GameObject newRatRef = Instantiate(ratPrefab, generateRandomPoint(true), ratPrefab.transform.rotation);
         Rat rat = newRatRef.GetComponent<Rat>();
 
         // set up the variables
@@ -123,12 +104,13 @@ public class RatNest : MonoBehaviour
 
     public Vector3 getInstruction()
     {
-        return generateRandomPoint(roamingPoints);
+        return generateRandomPoint(false);
     }
 
-    private Vector3 generateRandomPoint(Vector3[] points)
+    
+    private Vector3 generateRandomPoint(bool isSpawn)
     {
-        int randomIndex = Random.Range(0, points.Length);
-        return points[randomIndex];
+        int rIndex = isSpawn ? Random.Range(0, points.Length - 4) : Random.Range(0, points.Length);
+        return points[rIndex];
     }
 }
