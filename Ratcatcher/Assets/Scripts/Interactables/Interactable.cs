d660c13fea9e3d4cf31fa20df4f6fb5030dea363
sp103 <Simonpaton01@outlook.com>
2022-03-25 14:08:15 +0000

    added  lightswitch syncronisation

--- Ratcatcher/Assets/Scripts/Interactables/Interactable.cs
+++ Ratcatcher/Assets/Scripts/Interactables/Interactable.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Interactable : MonoBehaviour
+public class Interactable : NetworkBehaviour
 {
     public string displayText;
     public string disableText = "";
     protected bool interactive = false;
     protected bool disabled = false;
+    protected Collider playerCollider;
 
     // display the control text
     public virtual void OnTriggerEnter(Collider other)
@@ -16,6 +18,7 @@ public class Interactable : MonoBehaviour
         {
             FindObjectOfType<UserInterface>().setInfo(displayText, 10f);
             interactive = true;
+            playerCollider = other;
         }
         else
             FindObjectOfType<UserInterface>().setInfo(disableText, 10f);
--- Ratcatcher/Assets/Scripts/LightSwitch.cs
+++ Ratcatcher/Assets/Scripts/LightSwitch.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 public class LightSwitch : Interactable
 {
+    [SerializeField]
     public Light[] circuit;
+    [SyncVar]
     public bool powered = false;
+    [SyncVar]
     public bool isOn = false;
 
     // Update is called once per frame
@@ -15,9 +19,26 @@ public class LightSwitch : Interactable
         {
             FindObjectOfType<AudioManager>().Play("Flashlight");
 
-            if(powered)
-                changeLights();
+            if (powered)
+                playerCollider.gameObject.GetComponent<PlayerInteraction>().CmdLightChange(this);
+                //changeLights();
         }
+    }
+
+    [Command]
+    public void CmdChangeLights()
+    {
+        RpcChangeLights();
+    }
+
+    [ClientRpc]
+    public void RpcChangeLights()
+    {
+        isOn = !isOn;
+
+        // turn all the lights on or off
+        foreach (Light l in circuit)
+            l.intensity = isOn ? l.intensity = 1 : l.intensity = 0;
     }
 
     // switch on or off lights in circuit
